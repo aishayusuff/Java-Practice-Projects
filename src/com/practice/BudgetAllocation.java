3cbfa21e08f037cda2945d6737c41a3eedd042f5
Aisha Yusuff <aishatyusuffe@gmail.com>
2022-12-20 13:42:26 +0000

    Adds Comments

--- src/com/practice/BudgetAllocation.java
+++ src/com/practice/BudgetAllocation.java
@@ -7,29 +7,33 @@ import java.util.Scanner;
 public class BudgetAllocation {
 
 	public static void main(String[] args) {
+		// Accept input from user
+
 		System.out.println("How much can you spend this month?");
 		Scanner scanner = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("0.00");
+
+		// variables
 		double spendingTotal = scanner.nextDouble();
 		double sumOfProportions = 0;
-		int i = 0;
-
+		int index = 0;
 		ArrayList<Double> proportions = new ArrayList<>();
+
+		// Accepts percentage proportions from user
 		System.out.println("Enter the proportions for your mothly expenses:");
 		System.out.println("NOTE: Total proportion must not exceed 100%!");
 
-		//	Handling user inputs for proportion
-
+		// Handles user inputs for proportion
 		do {
 			double proportionInput = scanner.nextDouble();
 			proportions.add(proportionInput);
-			System.out.println("Your proportion input " + (i + 1) + " is " + proportionInput);
-			sumOfProportions += proportions.get(i);
-			i++;
+			System.out.println("Your proportion input " + (index + 1) + " is " + proportionInput);
+			sumOfProportions += proportions.get(index);
+			index++;
 		} while (sumOfProportions <= 100);
 		scanner.close();
 
-		// Handling sum of proportions OVER 100%
+		// Handles sum of proportions OVER 100%
 
 		if (sumOfProportions > 100) {
 			double cumulativeSum = 0.0;
@@ -39,7 +43,7 @@ public class BudgetAllocation {
 			proportions.set(proportions.size() - 1, 100 - cumulativeSum);
 		}
 
-		// Allocating spending by inputed proportions
+		// Allocates spending amount by inputed proportions
 		for (double proportion : proportions) {
 			double expense = (proportion * spendingTotal) / 100.0;
 			System.out.println("Expense for " + proportion + " is " + df.format(expense));
